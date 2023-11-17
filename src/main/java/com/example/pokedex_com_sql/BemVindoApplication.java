7e1b12f64724f406e5cfeb81ce7a6f41a53266a7
andrei <108534182+AndreiMartinsCoelho@users.noreply.github.com>
2023-11-17 16:03:26 -0300

    :bug: Melhorias de bugs e geral

--- src/main/java/com/example/pokedex_com_sql/BemVindoApplication.java
+++ src/main/java/com/example/pokedex_com_sql/BemVindoApplication.java
@@ -13,7 +13,7 @@ public class BemVindoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("bem-vindo.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1440, 770);
         stage.setTitle("Pokedex");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/pokedex_com_sql/Controller/ListPokeController.java
+++ src/main/java/com/example/pokedex_com_sql/Controller/ListPokeController.java
@@ -1,21 +1,29 @@
 package com.example.pokedex_com_sql.Controller;
 
+import com.example.pokedex_com_sql.HelloApplication;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import com.example.pokedex_com_sql.Model.PokemonModel;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import com.example.pokedex_com_sql.Model.DB;
+import javafx.stage.Stage;
 
 public class ListPokeController {
 
@@ -47,6 +55,9 @@ public class ListPokeController {
 
     @FXML
     private TableColumn<PokemonModel, String> clNomeTipo;
+
+    @FXML
+    private AnchorPane painelPrefeito;
 
     ResultSet rs;
     ObservableList<PokemonModel> lista = FXCollections.observableArrayList();
@@ -86,6 +97,25 @@ public class ListPokeController {
             clTabela.setItems(lista);
         } catch (SQLException e) {
             System.out.println("Erro ao listar os pokemons"+ e.getMessage());
+        }
+    }
+
+    @FXML
+    void onVoltar(ActionEvent event) {
+        System.out.println("Entrou");
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(HelloApplication.class.getResource("hello-view.fxml"));
+            Scene painel2 = new Scene(loader.load(), 1440, 770);
+
+            Stage stage = (Stage) painelPrefeito.getScene().getWindow();
+            stage.setTitle("Adicionar Pokemon!");
+            stage.setScene(painel2);
+            stage.show();
+
+        } catch (IOException e) {
+            System.out.println("Erro ao carregar a tela de adicionar pokemon");
+            e.printStackTrace();
         }
     }
 
